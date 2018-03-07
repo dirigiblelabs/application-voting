c5698dbc09458e3dda25590c02974da05a7fdd53
ThuF <jordan.p.pavlov@gmail.com>
2018-03-07 10:11:19 +0200

    Adds admin management page

--- voting/api/admin/results.js
+++ voting/api/admin/results.js
@@ -1,11 +1,14 @@
 var rs = require('http/v3/rs');
 var query = require('db/v3/query');
+var update = require('db/v3/update');
 
 var getProposalsSql = 'select PROPOSAL_NAME, PROPOSAL_IMAGE from PROPOSALS';
 
 var getResultsSql = 'select PROPOSAL_NAME, count(*) as VOTES from PROPOSALS'
 	+ ' join VOTES on VOTE_PROPOSAL_ID = PROPOSAL_ID'
 	+ ' group by (PROPOSAL_NAME) order by VOTES';
+
+var deleteResultsSql = 'delete from VOTES';
 
 rs.service()
 	.resource('')
@@ -27,5 +30,10 @@ rs.service()
 				return b.VOTES - a.VOTES;
 			});
 			response.println(JSON.stringify(votingResults));
+		})
+		.delete(function(ctx, request, response) {
+			update.execute(deleteResultsSql);
+			response.println('');
+			response.status(200);
 		})
 .execute();
